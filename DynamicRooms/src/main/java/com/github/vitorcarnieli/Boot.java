c827e5326efdbb1532497852bc2ef838dfb0c4b4
vitorcarnieli <vitorcarnieli135@gmail.com>
2023-11-08 09:29:45 -0300

    aa

---
+++ DynamicRooms/src/main/java/com/github/vitorcarnieli/Boot.java
@@ -0,0 +1,12 @@
+package com.github.vitorcarnieli;
+
+public class Boot {
+
+    public static void main(String[] args) {
+        System.out.println("|===========================================|");
+        System.out.println("|============== DYNAMIC ROOMS ==============|");
+        System.out.println("|===========================================|");
+
+    }
+
+}
--- dinamicRooms/src/main/java/com/github/vitorcarnieli/Boot.java
+++
@@ -1,9 +0,0 @@
-package com.github.vitorcarnieli;
-
-public class Boot {
-
-    public static void main(String[] args) {
-
-    }
-
-}
